23c464fe0927cea7926d6d00d2b53b2ff0409a6c
Hans Song <hans.m.song@gmail.com>
2021-05-19 17:35:33 +1000

    parse constants from env

--- constants.py
+++ constants.py
@@ -1,10 +1,38 @@
 import logging
+import os
 
-DEBUG = True
-DEBUG_LEVEL = logging.DEBUG
+
+def get_log_level():
+    log_level = os.environ.get("DEBUG_LEVEL", "INFO").upper()
+    if log_level == "CRITICAL":
+        return logging.CRITICAL
+    if log_level == "ERROR":
+        return logging.ERROR
+    if log_level == "WARNING":
+        return logging.WARNING
+    if log_level == "INFO":
+        return logging.INFO
+    if log_level == "DEBUG":
+        return logging.DEBUG
+    if log_level == "NOTSET":
+        return logging.NOTSET
+    if log_level == "DEBUG":
+        return logging.DEBUG
+    else:
+        return logging.INFO
+
+
+def get_env_bool(key: str, default: str = "true"):
+    return os.environ.get(key, default).lower() == "true"
+
+
+DEBUG = get_env_bool("DEBUG")
+DEBUG_LEVEL = get_log_level()
 
 WINDOW_NAME = "NEON DRIVE"
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
 
-SKIP_ASSET_LOAD = False
+SKIP_ASSET_LOAD = get_env_bool("SKIP_ASSET_LOAD", "false")
+
+CAPTURE_MOUSE = get_env_bool("CAPTURE_MOUSE")
