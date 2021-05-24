5fc38b7c42611aeeeaf8a3f1e5991cc069ddaa82
Hans Song <hans.m.song@gmail.com>
2021-05-24 11:17:27 +1000

    return car to center

--- entities/Car.py
+++ entities/Car.py
@@ -15,13 +15,14 @@ class Car(Entity):
     move_speed = 0.1
     strafe_speed = 0.05
     drift_speed = 0.4
+    return_speed = 0.01
     velocity = 0.2
     acceleration = 0.05
 
     velocity_min = 0.1
     velocity_max = 0.8
-    strafe_max = 5
-    strafe_min = -5
+    offset_max = 5
+    offset_min = -5
     yaw_max = 35
     yaw_min = -35
 
@@ -41,39 +42,57 @@ class Car(Entity):
 
         # y is z in context of view
         x, z, y = self.position
+        yaw = self.drift_yaw
 
         # horizontal movement
         if keyboard.state["A"]:
-            x = min(x + self.strafe_speed, self.strafe_max)
-            self.drift_yaw = clamp(
-                self.drift_yaw + self.drift_speed,
+            x = min(x + self.strafe_speed, self.offset_max)
+            yaw = clamp(
+                yaw + self.drift_speed,
                 limit_max=self.yaw_max,
                 limit_min=self.yaw_min,
             )
         elif keyboard.state["D"]:
-            x = max(x - self.strafe_speed, self.strafe_min)
-            self.drift_yaw = clamp(
-                self.drift_yaw - self.drift_speed,
+            x = max(x - self.strafe_speed, self.offset_min)
+            yaw = clamp(
+                yaw - self.drift_speed,
                 limit_max=self.yaw_max,
                 limit_min=self.yaw_min,
             )
 
         # vertical movement
         if keyboard.state["W"]:
-            y = min(y + self.move_speed, self.strafe_max)
+            y = min(y + self.move_speed, self.offset_max)
             self.velocity = clamp(
                 self.velocity + self.acceleration,
                 limit_max=self.velocity_max,
                 limit_min=self.velocity_min,
             )
         elif keyboard.state["S"]:
-            y = max(y - self.move_speed, self.strafe_min)
+            y = max(y - self.move_speed, self.offset_min)
             self.velocity = clamp(
                 self.velocity - self.acceleration,
                 limit_max=self.velocity_max,
                 limit_min=self.velocity_min,
             )
 
+        # eventually return to center
+        if y > 0:
+            y = max(y - self.return_speed, 0)
+        elif y < 0:
+            y = min(y + self.return_speed, 0)
+
+        if x > 0:
+            x = max(x - self.return_speed, 0)
+        elif x < 0:
+            x = min(x + self.return_speed, 0)
+
+        if yaw > 0:
+            yaw = max(yaw - 10 * self.return_speed, 0)
+        elif yaw < 0:
+            yaw = min(yaw + 10 * self.return_speed, 0)
+
+        self.drift_yaw = yaw
         self.position = [x, z, y]
 
     def render(self, view: View = None):
